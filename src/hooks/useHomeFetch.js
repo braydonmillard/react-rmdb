644051802df081f477ce8c02031b6f6f941b4293
BraydonMillard <braydonmillard@users.noreply.github.com>
2022-02-17 17:11:08 -0500

    Implemented search functionality

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -11,6 +11,7 @@ const initialState = {
 };
 
  export const useHomeFetch = () => {
+        const [searchTerm, setSearchTerm] = useState('');
         const [state, setState] = useState(initialState);
         const [loading, setLoading] = useState(false);
         const [error, setError] = useState(false);
@@ -34,11 +35,11 @@ const initialState = {
 
         };
     
-        // Initial render
+        // Initial and search
         useEffect(() => {
-            fetchMovies(1);
-    
-        }, []);
+            setState(initialState);
+            fetchMovies(1, searchTerm);
+        }, [searchTerm]);
 
-        return { state, loading, error };
+        return { state, loading, error, searchTerm, setSearchTerm };
     };
